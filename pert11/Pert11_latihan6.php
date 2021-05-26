8721788fd2144a98350bea3e40e3371029ad68c4
thamjhon <71577308+thamjhon@users.noreply.github.com>
2021-05-26 10:04:42 +0700

    Delete pert11 directory

--- pert11/Pert11_latihan6.php
+++
@@ -1,9 +0,0 @@
-<?php
- $con=mysqli_connect("localhost","root","");
- mysqli_select_db($con,"lat_dbase");
- $hasil=mysqli_query($con,"select * from tbl_mhs");
- While($data=mysqli_fetch_array($hasil))
- {
- echo "$data[FirstName] $data[LastName] $data[Age]<br>";
- }
-?> 
